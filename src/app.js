f65c2d8f59dd6b3a846562caca8033a331dd998a
YahirCamargo <ycamargocalderon@gmail.com>
2025-12-23 18:27:41 -0600

    Cambio en js, print de pruebas borrado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('Vite + Supabase OK');
-
 const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_ANON_KEY
